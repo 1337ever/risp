d20a0ded00063578c41da2a8cb6c13b43b088f19
Ever Gold <ever1337@protonmail.ch>
2021-09-13 20:59:07 -0700

    repl

--- main.rs
+++ main.rs
@@ -1,3 +1,31 @@
+extern crate rustyline;
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+
 fn main() {
-    println!("test");
+    let mut rl = Editor::<()>::new();
+    if rl.load_history(".risp-history").is_err() {
+        eprintln!("No history found.");
+    }
+
+    loop {
+        let readline = rl.readline("()> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(&line);
+                rl.save_history(".risp-history").unwrap();
+                if line.len() > 0 {
+                    println!("{}", line);
+                }
+            }
+            Err(ReadlineError::Interrupted) => continue,
+            Err(ReadlineError::Eof) => break,
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
 }
